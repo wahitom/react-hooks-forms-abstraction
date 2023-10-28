5a0bb0c82d36b7680d2e288d162ed5ea98371639
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-10-29 01:39:28 +0300

    Completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,57 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  //add state to your form
+  //add items in state as object so we can add several at onde and use them later
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+    admin: false,
+  });
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  //functon for targeting the input value through onChange
+  function handleChange(event) {
+    const name = event.target.name; //this is each name ie first and last
+    let value = event.target.value; // whatever our input is
+
+    // adding a checkbox with the cheked propert
+    if (event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
+
+    //setter function for the data
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  //submit function
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
   }
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="firstName"
+        onChange={handleChange}
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        name="lastName"
+        onChange={handleChange}
+        value={formData.lastName}
+      />
+      <input
+        type="checkbox"
+        name="admin"
+        onChange={handleChange}
+        checked={formData.admin}
+      />
       <button type="submit">Submit</button>
     </form>
   );
